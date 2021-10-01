2ffc8c30081c0641caaa47dc044dddec5d503d0b
anddever <anddever@gmail.com>
2021-10-01 23:46:04 +0700

    Add loaded image to phone gallery

--- app/src/main/java/ru/anddever/androidimagecompressor/Utils.kt
+++ app/src/main/java/ru/anddever/androidimagecompressor/Utils.kt
@@ -1,7 +1,10 @@
 package ru.anddever.androidimagecompressor
 
 import android.content.Context
+import android.content.Intent
 import android.net.Uri
+import android.os.Build
+import android.os.Environment
 import java.io.File
 import java.io.FileOutputStream
 
@@ -23,6 +26,35 @@ class Utils {
                 }
             }
             return outputFile
+        }
+
+        fun getAlbumDir(context: Context): File? {
+            var storageDir: File? = null
+            if (Environment.MEDIA_MOUNTED == Environment.getExternalStorageState()) {
+                storageDir = if (Build.VERSION.SDK_INT >= 29) {
+                    context.getExternalFilesDir(Environment.DIRECTORY_PICTURES)
+                } else {
+                    File(
+                        Environment.getExternalStoragePublicDirectory(
+                            Environment.DIRECTORY_PICTURES
+                        ), context.getString(R.string.app_name)
+                    )
+                }
+                if (!storageDir!!.mkdirs()) {
+                    if (!storageDir.exists()) {
+                        return null
+                    }
+                }
+            }
+            return storageDir
+        }
+
+        fun addPicToGallery(context: Context, mCurrentPhotoPath: String) {
+            val mediaScanIntent = Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE)
+            val f = File(mCurrentPhotoPath)
+            val contentUri = Uri.fromFile(f)
+            mediaScanIntent.data = contentUri
+            context.sendBroadcast(mediaScanIntent)
         }
     }
 }
