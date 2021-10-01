8276f192f21df3d6623f31d826b524c9939d77c2
anddever <anddever@gmail.com>
2021-10-01 23:04:01 +0700

    Load target image to preview

--- app/src/main/java/ru/anddever/androidimagecompressor/MainActivity.kt
+++ app/src/main/java/ru/anddever/androidimagecompressor/MainActivity.kt
@@ -1,22 +1,57 @@
 package ru.anddever.androidimagecompressor
 
-import androidx.appcompat.app.AppCompatActivity
+import android.net.Uri
 import android.os.Bundle
+import android.os.Environment
 import android.view.View
-import android.widget.Toast
-
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
+import com.squareup.picasso.Picasso
 import ru.anddever.androidimagecompressor.databinding.ActivityMainBinding
+import java.io.*
+import java.text.SimpleDateFormat
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
+
+    var binding: ActivityMainBinding? = null
+    private var imageFile: File? = null
+    private val getImageUri =
+        this.registerForActivityResult(ActivityResultContracts.GetContent()) { uri: Uri? ->
+            if (uri != null) {
+                imageFile = Utils.copyStreamToFile(this, uri)
+                if (imageFile != null) {
+                    val timeStamp: String =
+                        SimpleDateFormat("yyyyMMdd_HHmmss", Locale.getDefault()).format(Date())
+                    val storageDir: File? = getExternalFilesDir(Environment.DIRECTORY_PICTURES)
+                    File.createTempFile(
+                        "JPEG_${timeStamp}_",
+                        ".jpg",
+                        storageDir
+                    ).apply {
+                        var currentPhotoPath: String = absolutePath
+                        val byteArray = ByteArray(imageFile!!.length().toInt())
+                        val bis = BufferedInputStream(FileInputStream(imageFile))
+                        val dis = DataInputStream(bis)
+                        dis.readFully(byteArray)
+                        val outputStream = FileOutputStream(this)
+                        outputStream.write(byteArray)
+                        if (imageFile != null) {
+                            Picasso.get().load(imageFile!!).into(binding!!.imageView)
+                        }
+                    }
+                }
+            }
+        }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val binding: ActivityMainBinding =
-            DataBindingUtil.setContentView(this, R.layout.activity_main)
-        binding.activity = this
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        binding!!.activity = this
     }
 
     fun onPickProblemImageClick(view: View) {
-        Toast.makeText(this, "test", Toast.LENGTH_LONG).show()
+        getImageUri.launch("image/*")
     }
 }
