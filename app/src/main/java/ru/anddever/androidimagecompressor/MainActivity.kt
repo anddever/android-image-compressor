6a6a7d5d2ac02e0e2eebbb49ad71801341eb0034
anddever <anddever@gmail.com>
2021-10-01 22:21:04 +0700

    Add views and click handler stub

--- app/src/main/java/ru/anddever/androidimagecompressor/MainActivity.kt
+++ app/src/main/java/ru/anddever/androidimagecompressor/MainActivity.kt
@@ -2,10 +2,21 @@ package ru.anddever.androidimagecompressor
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+
+import androidx.databinding.DataBindingUtil
+import ru.anddever.androidimagecompressor.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding: ActivityMainBinding =
+            DataBindingUtil.setContentView(this, R.layout.activity_main)
+        binding.activity = this
+    }
+
+    fun onPickProblemImageClick(view: View) {
+        Toast.makeText(this, "test", Toast.LENGTH_LONG).show()
     }
 }
